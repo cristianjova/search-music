853ce690e048ee5a4b375913c35cc1a8665320f6
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-05 18:45:16 -0300

    added index, navbar and tracks component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layouts/Navbar';
+import Index from './components/layouts/Index';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Index} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 }
 
 export default App;
